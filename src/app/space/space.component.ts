f33519796055e696e2371b8d565e75e9c7303e4f
bgolyoo <bgolyoo@gmail.com>
2019-09-04 00:58:06 +0200

    4 files modified

--- src/app/space/space.component.ts
+++ src/app/space/space.component.ts
@@ -9,13 +9,17 @@ import { CONSTELLATIONS } from './constellations';
 export class SpaceComponent implements AfterViewInit {
 
   stars = [];
+  lines = [];
+  private hostWidth;
+  private hostHeight;
 
   constructor(private elRef: ElementRef) { }
 
   ngAfterViewInit() {
-    // this.createStars(200, 1);
-    // this.createStars(50, 2);
-    // this.createStars(5, 3);
+    this.hostWidth = this.elRef.nativeElement.clientWidth;
+    this.hostHeight = this.elRef.nativeElement.clientHeight;
+    this.createStars(200, 1);
+    this.createStars(50, 2);
     this.createConstellations();
   }
 
@@ -30,11 +34,13 @@ export class SpaceComponent implements AfterViewInit {
       const opacityVal = (Math.floor(Math.random() * 11)) / 10;
       const style = `rgba(${colorVal01},${colorVal02},255,${opacityVal})`;
       this.stars.push({
-        'background-color': style,
-        'left': `${starLeft}px`,
-        'top': `${starTop}px`,
-        'width': `${starSize}px`,
-        'height': `${starSize}px`,
+        style: {
+          'background-color': style,
+          'left': `${starLeft}px`,
+          'top': `${starTop}px`,
+          'width': `${starSize}px`,
+          'height': `${starSize}px`
+        }
       });
     }
   }
@@ -42,26 +48,111 @@ export class SpaceComponent implements AfterViewInit {
   createConstellations() {
     const hostHeight = this.elRef.nativeElement.clientHeight;
     const hostWidth = this.elRef.nativeElement.clientWidth;
-    const radius = Math.sqrt(Math.pow(hostHeight,2) + Math.pow(hostWidth,2));
-    console.log(hostHeight,hostWidth,radius);
+    const radius = Math.sqrt(Math.pow(hostHeight, 2) + Math.pow(hostWidth, 2));
     const constellations: { constellations: any, minmax: any } = CONSTELLATIONS;
+
+    const starLeft = (ra, raMin, raMax) => (ra / (ra > 0 ? Math.abs(raMax) : ra < 0 ? Math.abs(raMin) : 0) * (radius / 2)) + (hostWidth / 2);
+    const starBottom = (dec, decMin, decMax) => (dec / (dec > 0 ? Math.abs(decMax) : dec < 0 ? Math.abs(decMin) : 0) * (radius / 2)) + (hostHeight / 2);
+    const opacity = (mag, magMax) => mag / magMax;
+    const size = (mag, magMax, sizeMax) => (opacity(mag, magMax) * sizeMax - 1) + 1;
+    const bgStyle = (opacity) => `rgba(255,255,255,${opacity})`;
+
+    const addStar = (star) => this.stars.push({
+      style: {
+        'background-color': star.bgStyle,
+        'left': `${star.left}px`,
+        'bottom': `${star.bottom}px`,
+        'width': `${star.size}px`,
+        'height': `${star.size}px`
+      },
+      label: star.label
+    });
+    const createStar = (ra, raMin, raMax, dec, decMin, decMax, mag, magMax, sizeMax, symbol) => ({
+      left: starLeft(ra, raMin, raMax),
+      bottom: starBottom(dec, decMin, decMax),
+      bgStyle: bgStyle(opacity(mag, magMax)),
+      size: size(mag, magMax, 4),
+      label: symbol
+    });
+
     constellations.constellations.forEach(constellation => {
       constellation.groups.forEach(group => {
-        group.forEach(item => {
-          const starLeft = (item.ra_north / (item.ra_north > 0 ? Math.abs(constellations.minmax.ra_north_max) : item.ra_north < 0 ? Math.abs(constellations.minmax.ra_north_min) : 0) * (radius / 2)) + (hostWidth / 2);
-          const starBottom = (item.dec_north / (item.dec_north > 0 ? Math.abs(constellations.minmax.dec_north_min) : item.dec_north < 0 ? Math.abs(constellations.minmax.dec_north_max) : 0) * (radius / 2)) + (hostHeight / 2);
-          const opacityVal = item.mag / constellations.minmax.mag_max;
-          const style = `rgba(255,255,255,${opacityVal})`;
-          this.stars.push({
-            'background-color': style,
-            'left': `${starLeft}px`,
-            'bottom': `${starBottom}px`,
-            'width': `${item.mag / constellations.minmax.mag_max * 3}px`,
-            'height': `${item.mag / constellations.minmax.mag_max * 3}px`,
+        let lastStar;
+        group.forEach((item, index) => {
+
+          if (!group[index + 1]) {
+            if (lastStar) {
+              addStar(lastStar);
+            }
+            return;
+          }
+
+          const fromStar = lastStar ? lastStar : createStar(
+            item.ra_north,
+            constellations.minmax.ra_north_min,
+            constellations.minmax.ra_north_max,
+            item.dec_north,
+            constellations.minmax.dec_north_min,
+            constellations.minmax.dec_north_max,
+            item.mag,
+            constellations.minmax.mag_max,
+            3,
+            item.symbol
+          );
+
+          const toStar = createStar(
+            group[index + 1].ra_north,
+            constellations.minmax.ra_north_min,
+            constellations.minmax.ra_north_max,
+            group[index + 1].dec_north,
+            constellations.minmax.dec_north_min,
+            constellations.minmax.dec_north_max,
+            group[index + 1].mag,
+            constellations.minmax.mag_max,
+            3,
+            group[index + 1].symbol
+          );
+
+          const a = Math.max(fromStar.left, toStar.left) - Math.min(fromStar.left, toStar.left);
+          const b = Math.max(fromStar.bottom, toStar.bottom) - Math.min(fromStar.bottom, toStar.bottom);
+
+          const triangle = this.randomTriangle({ min: a, max: a }, { min: b, max: b });
+
+          const rotate = fromStar.left < toStar.left
+            ? fromStar.bottom < toStar.bottom
+              ? triangle.betaDegree * -1
+              : triangle.betaDegree
+            : fromStar.bottom < toStar.bottom
+              ? 180 + triangle.betaDegree
+              : 180 - triangle.betaDegree;
+
+          this.lines.push({
+            bottom: `${fromStar.bottom}px`,
+            left: `${fromStar.left}px`,
+            transform: `rotate(${rotate}deg)`,
+            width: `${triangle.c}px`
           });
+
+          addStar(fromStar);
+          lastStar = toStar;
         });
       });
     });
   }
 
+
+  private randomTriangle(aSide: { min?: number, max: number }, bSide: { min?: number, max: number }): { alphaDegree: number, betaDegree: number, a: number, b: number, c: number } {
+    const degree = (radian: number) => radian * 180 / Math.PI;
+    const a = aSide.min === aSide.max ? aSide.max : aSide.min ? this.randomNumber(aSide.max, aSide.min) : this.randomNumber(aSide.max);
+    const b = bSide.min === bSide.max ? bSide.max : bSide.min ? this.randomNumber(bSide.max, bSide.min) : this.randomNumber(bSide.max);
+    const alphaRadian = Math.atan(a / b);
+    const alphaDegree = degree(alphaRadian);
+    const betaDegree = 90 - alphaDegree;
+    const c = Math.abs(a / Math.sin(alphaRadian));
+    return { alphaDegree, betaDegree, a, b, c };
+  }
+
+  private randomNumber(max: number, min = 0): number {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 }
